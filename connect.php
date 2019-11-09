d2b72df0370e9b86908682bbdae26de98c4a02d0
HellMemories <chinhndgch18797@fpt.edu.vn>
2019-11-09 13:09:31 +0700

    changed

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php 
-//  $dbconn3 = pg_connect("host=ec2-50-19-222-129.compute-1.amazonaws.com port=5432 dbname=d59drdf1vuv8v9 user=qaotrimwoaboes password=9530aa2adfbe2297542280a0d4fe9fc0b4ca4f020c4a483a94f75e5ba15035e7");
+//  $dbconn3 = pg_connect("host=ec2-174-129-231-100.compute-1.amazonaws.com; port=5432; dbname=d689jn5845vech', 'lxkqpzmtrvjyql', '4a9c572ed86cc81255ec1fc9f513ea0ab66236985cf88e1199414b6e9fbe3998");
  $pdo= new PDO('pgsql:host=ec2-174-129-231-100.compute-1.amazonaws.com; port=5432; dbname=d689jn5845vech', 'lxkqpzmtrvjyql', '4a9c572ed86cc81255ec1fc9f513ea0ab66236985cf88e1199414b6e9fbe3998');
 
 ?>
